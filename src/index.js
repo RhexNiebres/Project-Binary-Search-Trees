863efbf93e7d802130b1beead9738eda500e8203
RhexNiebres <rhexniebres@gmail.com>
2025-01-10 19:32:31 -0800

    build class of Node and Tree with its attibutes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import './styles.css'; // Import the CSS file
-console.log('Webpack setup complete!');
+class Node {
+    constructor(data, left = null, right = null) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+  
+  // Tree Class
+  class Tree {
+    constructor(array) {
+      this.root = this.buildTree([...new Set(array)].sort((a, b) => a - b));
+    }
+  
+}
+  
